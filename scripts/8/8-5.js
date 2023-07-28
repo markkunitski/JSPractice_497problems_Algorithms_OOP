492d2dab6da87726516ffe123dd0cff9a2c662d2
markkunitski <markkmpya@gmail.com>
2023-07-28 20:42:47 +0200

    28.07 working day
    Sovled some more problems

--- scripts/8/8-5.js
+++ scripts/8/8-5.js
@@ -1,66 +1,148 @@
 import * as selectors from "../../selectors.js";
 import pop from "../../selectors.js";
 1;
-let data = selectors.p1.innerText;
-function createList(letter, wordsArr) {
-  let parent = document.createElement("ul");
-  wordsArr.forEach((word) => {
-    let child = document.createElement("li");
-    child.textContent = word;
-    parent.appendChild(child);
-  });
-  let domElem = selectors.div;
-  let name = document.createElement("h2");
-  name.textContent = letter;
-  domElem.appendChild(name);
-  domElem.append(parent);
-}
-function dataFilter(elem) {
-  let arr = elem.toLowerCase().trim().split(" ");
-  let result = arr.map((elem) => {
-    if (
-      elem[elem.length - 1].toLowerCase() == elem[elem.length - 1].toUpperCase()
-    ) {
-      return elem.slice(0, elem.length - 1);
-    } else {
-      return elem;
-    }
-  });
-  return result;
-}
-const createObject = (data) => {
-  let result = [];
-  data.forEach((elem) => {
-    let firtsLetter = elem[0];
-    let targetIndex;
-    result.forEach((acc, index) => {
-      if (acc[firtsLetter]) {
-        targetIndex = index;
-        return;
-      }
-    });
-    if (targetIndex === undefined) {
-      let newO = { [firtsLetter]: [elem] };
-      result.push(newO);
-    } else if (!result[targetIndex][firtsLetter].includes(elem)){
-        result[targetIndex][firtsLetter].push(elem);
-    }
-    
-  });
-  result.forEach((elem) => {
-    let letter = Object.keys(elem);
-    let arr = elem[letter];
-    createList(letter, arr);
-  });
-};
-selectors.btn.addEventListener("click", function () {
-  let filtered = dataFilter(data);
-  createObject(filtered);
-});
+// let data = selectors.p1.innerText;
+// function createList(letter, wordsArr) {
+//   let parent = document.createElement("ul");
+//   wordsArr.forEach((word) => {
+//     let child = document.createElement("li");
+//     child.textContent = word;
+//     parent.appendChild(child);
+//   });
+//   let domElem = selectors.div;
+//   let name = document.createElement("h2");
+//   name.textContent = letter;
+//   domElem.appendChild(name);
+//   domElem.append(parent);
+// }
+// function dataFilter(elem) {
+//   let arr = elem.toLowerCase().trim().split(" ");
+//   let result = arr.map((elem) => {
+//     if (
+//       elem[elem.length - 1].toLowerCase() == elem[elem.length - 1].toUpperCase()
+//     ) {
+//       return elem.slice(0, elem.length - 1);
+//     } else {
+//       return elem;
+//     }
+//   });
+//   return result;
+// }
+// const createObject = (data) => {
+//   let result = [];
+//   data.forEach((elem) => {
+//     let firtsLetter = elem[0];
+//     let targetIndex;
+//     result.forEach((acc, index) => {
+//       if (acc[firtsLetter]) {
+//         targetIndex = index;
+//         return;
+//       }
+//     });
+//     if (targetIndex === undefined) {
+//       let newO = { [firtsLetter]: [elem] };
+//       result.push(newO);
+//     } else if (!result[targetIndex][firtsLetter].includes(elem)){
+//         result[targetIndex][firtsLetter].push(elem);
+//     }
+
+//   });
+//   result.forEach((elem) => {
+//     let letter = Object.keys(elem);
+//     let arr = elem[letter];
+//     createList(letter, arr);
+//   });
+// };
+// selectors.btn.addEventListener("click", function () {
+//   let filtered = dataFilter(data);
+//   createObject(filtered);
+// });
 2;
-// selectors.btn.addEventListener('click', function () {})
+// let curr = {
+//   month: 0,
+//   year: 2000,
+// };
+
+// const makeDays = (now, mutable) => {
+//   let parent = document.createElement("select");
+//   parent.name = "day";
+//   parent.id = "daySelect";
+//   let flag = document.querySelector("#daySelect");
+//   if (flag) {
+//     selectors.div.removeChild(flag);
+//   }
+//   let start = new Date(mutable.year, mutable.month + 1, 0);
+//   console.log(mutable);
+//   console.log(start + " start");
+//   for (let i = 1; i <= start.getDate(); i++) {
+//     let child = document.createElement("option");
+//     child.value = i;
+//     child.textContent = i;
+//     if (i == now) child.selected = true;
+//     parent.append(child);
+//   }
+
+//   selectors.div.prepend(parent);
+// };
+// const makeMonths = (now) => {
+//   let parent = document.createElement("select");
+//   parent.name = "month";
+//   parent.id = "monthSelect";
+//   let start = new Date(now.getFullYear(), 0, 1);
+//   for (let i = 0; i <= 11; i++) {
+//     let name = start.toLocaleString("en-US", { month: "long" });
+//     let child = document.createElement("option");
+//     child.value = i;
+//     child.textContent = name;
+//     if (i == now.getMonth()) child.selected = true;
+//     parent.append(child);
+//     start.setMonth(i + 1);
+//   }
+//   parent.addEventListener("change", (e) => {
+//     curr.month = Number(e.target.value);
+//     makeDays(1, curr);
+//     console.log(new Date(curr.year, curr.month));
+//   });
+//   selectors.div.append(parent);
+// };
+// const makeYears = (now, len) => {
+//   let parent = document.createElement("select");
+//   parent.name = "year";
+//   parent.id = "yearSelect";
+//   let start = new Date(now.getFullYear() - len, 0, 1);
+//   for (let i = 1; i <= len * 2; i++) {
+//     let name = start.toLocaleString("en-US", { year: "numeric" });
+//     let child = document.createElement("option");
+//     child.value = name;
+//     child.textContent = name;
+//     if (start.getFullYear() == now.getFullYear()) child.selected = true;
+//     parent.append(child);
+//     start.setFullYear(start.getFullYear() + 1);
+//   }
+//   parent.addEventListener("change", (e) => {
+//     curr.year = Number(e.target.value);
+//     makeDays(1, curr);
+//     console.log(new Date(curr.year, curr.month));
+//   });
+//   selectors.div.append(parent);
+// };
+// selectors.btn.addEventListener("click", function () {
+//   let date = new Date();
+//   curr.month = date.getMonth();
+//   curr.year = date.getFullYear();
+//   makeDays(date.getDate(), curr);
+//   makeMonths(date);
+//   makeYears(date, 10);
+// });
 3;
-// selectors.btn.addEventListener('click', function () {})
+function f5(){
+  for (let i = 1; i<=9; i +=2){
+    let symbol = String(i)
+    let result = symbol.padStart(i, symbol)
+    console.log(result)
+  }
+}
+selectors.btn.addEventListener('click', f5)
 4;
 // selectors.btn.addEventListener('click', function () {})
 5;
---
+++ scripts/8/8-6.js
@@ -0,0 +1,35 @@
+import * as selectors from "../../selectors.js";
+import pop from "../../selectors.js";
+1;
+// function getRandom() {
+//   return Math.floor(Math.random() * 256);
+// }
+// btn.addEventListener("click", function () {
+//   let target = selectors.p1;
+//   let targetSepareted = target.textContent.split('').map(elem=> `<span style="color: rgb(${getRandom()}, ${getRandom()}, ${getRandom()})">${elem}</span>`)
+//   target.innerHTML = ''
+//   targetSepareted.forEach(elem=> target.insertAdjacentHTML('beforeend',elem))
+// });
+2;
+// selectors.btn.addEventListener("click", function () {
+//   let data = selectors.p1.innerText.toLowerCase()
+//   .trim()
+//   .split(" ");
+//   let value = selectors.inp1.value.trim().toLowerCase()
+//   let targetIndex = data.indexOf(value)
+//   console.log(targetIndex)
+//   if (targetIndex === -1) return
+//   data[targetIndex] = `<span style="color: red">${data[targetIndex]}</span>`
+//   selectors.p1.innerHTML = ''
+//   data.forEach(elem=> selectors.p1.insertAdjacentHTML('beforeend', elem + ' '))
+// });
+3;
+selectors.btn.addEventListener('click', function () {
+   console.log(document.URL) 
+})
+4;
+// selectors.btn.addEventListener('click', function () {})
+5;
+// selectors.btn.addEventListener('click', function () {})
+6;
+// selectors.btn.addEventListener('click', function () {})
